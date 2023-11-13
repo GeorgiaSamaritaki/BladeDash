0e3bb366b5e664899db678f2a51e7bd7d66be049
GeaSam <samaritakigeorgia@gmail.com>
2023-11-13 16:05:11 +0100

    Sum 59

--- Source/SlashWorld/Private/Items/Item.cpp
+++ Source/SlashWorld/Private/Items/Item.cpp
@@ -4,25 +4,34 @@
 #include "Items/Item.h"
 #include "SlashWorld/DebugMacros.h"
 
-AItem::AItem(){
+AItem::AItem() {
 	PrimaryActorTick.bCanEverTick = true;
 }
 
 void AItem::BeginPlay(){
 	Super::BeginPlay();
 
+	int e = Avg<int32>(1, 3);
+	UE_LOG(LogTemp, Warning, TEXT("Avg of 1 and 3: %d"), e);
+}
+
+float AItem::TransformedSin(){
+
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos(){
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
 }
 
 void AItem::Tick(float DeltaTime){
 	Super::Tick(DeltaTime);
 
-	float MovementRate = 50.f;
-	float RotationRate = 45.f;
-
-	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
-	AddActorWorldRotation(FRotator(0.f, RotationRate* DeltaTime, 0.f));
+	RunningTime += DeltaTime;
 
 	DRAW_SPHERE_SingleFrame(GetActorLocation());
 	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f)
-}
 
+	FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
+	DRAW_POINT_SingleFrame(AvgVector);
+}
